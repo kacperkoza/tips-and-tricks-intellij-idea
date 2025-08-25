1762ae6c03c2eba58564a9deab9478fa096bc978
Kacper Koza <kacper.koza@allegro.com>
2025-08-25 08:34:01 +0200

    init

---
+++ src/test/kotlin/com/presentation/intellij/tips/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.presentation.intellij.tips
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
